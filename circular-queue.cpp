b08c5a0b526f674412417f1e472320c9116dd570
Jacob <jbodera@uwaterloo.ca>
2021-06-29 17:53:29 -0400

    Implemented some functions. Don't know if they work yet.

--- circular-queue.cpp
+++ circular-queue.cpp
@@ -5,51 +5,73 @@ const CircularQueue::QueueItem CircularQueue::EMPTY_QUEUE = -999;
 
 CircularQueue::CircularQueue()
 {
-
+    capacity_ = 16;
+    size_ = 0;
+    head_ = 0;
+    tail_ = 0;
+    items_ = new QueueItem[capacity_];
 }
 
 CircularQueue::CircularQueue(unsigned int capacity)
 {
-
+    capacity_ = capacity;
+    size_ = 0;
+    head_ = 0;
+    tail_ = 0;
+    items_ = new QueueItem[capacity_];
 }
 
 CircularQueue::~CircularQueue()
 {
-
+    for (int i = 0; i < size_; i++) {
+        items_[i] = NULL;
+    }
+    delete[] items_;
 }
 
 bool CircularQueue::empty() const
 {
-
+    return (size_ == 0);
 }
 
 bool CircularQueue::full() const
 {
-
+    return (size_ == capacity_);
 }
 
 int CircularQueue::size() const
 {
-
+    return size_;
 }
 
 bool CircularQueue::enqueue(QueueItem value)
 {
-
-
+    if (full())
+        return false;
+    items_[tail_] = value;
+    tail_ = (tail_ + 1) % capacity_;
+    ++size_;
+    return true;
 }
 
 CircularQueue::QueueItem CircularQueue::dequeue()
 {
-
+    if (empty())
+        return EMPTY_QUEUE;
+    QueueItem value = items_[head_];
+    head_ = (head_ + 1) % capacity_;
+    --size_;
+    return value;
 }
 
 CircularQueue::QueueItem CircularQueue::peek() const
 {
-
+    return items_[head_];
 }
 
 void CircularQueue::print() const
 {
+    for (int i = 0; i < size_; i++) {
 
+    }
 }
