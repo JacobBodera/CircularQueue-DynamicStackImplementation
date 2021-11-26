4add5d357d70781153ecb6187dbe8b8d9fb19325
Jacob <jbodera@uwaterloo.ca>
2021-11-26 15:36:30 -0500

    Comments added

--- circular-queue.cpp
+++ circular-queue.cpp
@@ -3,6 +3,7 @@
 
 const CircularQueue::QueueItem CircularQueue::EMPTY_QUEUE = -999;
 
+// default constructor for CircularQueue class
 CircularQueue::CircularQueue()
 {
     capacity_ = 16;
@@ -11,7 +12,7 @@ CircularQueue::CircularQueue()
     tail_ = 0;
     items_ = new QueueItem[capacity_];
 }
-
+// parametric constructor for CircularQueue class
 CircularQueue::CircularQueue(unsigned int capacity)
 {
     capacity_ = capacity;
@@ -20,7 +21,7 @@ CircularQueue::CircularQueue(unsigned int capacity)
     tail_ = 0;
     items_ = new QueueItem[capacity_];
 }
-
+// deconstructor for CircularQueue class
 CircularQueue::~CircularQueue()
 {
     for (int i = 0; i < size_; i++) {
@@ -28,22 +29,22 @@ CircularQueue::~CircularQueue()
     }
     delete[] items_;
 }
-
+// outputs whether or not the queue is empty
 bool CircularQueue::empty() const
 {
     return (size_ == 0);
 }
-
+// outputs whether the queue is full
 bool CircularQueue::full() const
 {
     return (size_ == capacity_);
 }
-
+// outputs the current size of the queue
 int CircularQueue::size() const
 {
     return size_;
 }
-
+// adds a queue item to the end of the queue if it is not full
 bool CircularQueue::enqueue(QueueItem value)
 {
     if (full())
@@ -63,14 +64,14 @@ CircularQueue::QueueItem CircularQueue::dequeue()
     --size_;
     return value;
 }
-
+// removes the item at the front of the queue and returns it
 CircularQueue::QueueItem CircularQueue::peek() const
 {
     if (empty())
         return EMPTY_QUEUE;
     return items_[head_];
 }
-
+// prints a representation of the queue
 void CircularQueue::print() const
 {
     for (int i = 0; i < size_; i++) {
--- dynamic-stack.cpp
+++ dynamic-stack.cpp
@@ -3,6 +3,7 @@
 
 const DynamicStack::StackItem DynamicStack::EMPTY_STACK = -999;
 
+// default constructor for the DynamicStack class
 DynamicStack::DynamicStack()
 {
     init_capacity_ = 16;
@@ -10,15 +11,15 @@ DynamicStack::DynamicStack()
     size_ = 0;
     items_ = new StackItem[capacity_];
 }
-
+// parametric constructor for the DynamicStack class
 DynamicStack::DynamicStack(unsigned int capacity)
 {
-    size_ = 0;
     init_capacity_ = capacity;
     capacity_ = capacity;
+    size_ = 0;
     items_ = new StackItem[capacity_];
 }
-
+// deconstructor for the DynamicStack class
 DynamicStack::~DynamicStack()
 {
     for (int i = 0; i < capacity_; i++){
@@ -26,17 +27,17 @@ DynamicStack::~DynamicStack()
     }
     delete[] items_;
 }
-
+// tests whether the stack is empty
 bool DynamicStack::empty() const
 {
     return (size_ == 0);
 }
-
+// outputs the size of the stack
 int DynamicStack::size() const
 {
     return size_;
 }
-
+// adds a stack item to the top of the stack
 void DynamicStack::push(StackItem value)
 {
     if (size_ == capacity_) {
@@ -45,7 +46,7 @@ void DynamicStack::push(StackItem value)
     items_[size_] = value;
     ++size_;
 }
-
+// removes and output the top value of the stack
 DynamicStack::StackItem DynamicStack::pop()
 {
     if (size_ == 0)
@@ -57,28 +58,28 @@ DynamicStack::StackItem DynamicStack::pop()
     --size_;
     return items_[size_];
 }
-
+// outputs the top value of the stack
 DynamicStack::StackItem DynamicStack::peek() const
 {
     if (size_ == 0)
         return EMPTY_STACK;
     return items_[size_ - 1];
 }
-
+// prints a visual representation of the stack
 void DynamicStack::print() const
 {
-    for (int i = 0; i < size_; i++){
+    for (int i = size_ - 1; i >= 0; i--){
         std::cout << "[ " << items_[i] << " ]" << std::endl;
     }
 }
-
+// copies the existing stack into a new stack with a larger/smaller capacity
 void DynamicStack::copyToNew(StackItem items, StackItem *newItems)
 {
     for (int i = 0; i < size_; i++) {
         newItems[i] = items_[i];
     }
 }
-
+// doubles the capacity of the stack to account for adding more values than the existing capacity
 void DynamicStack::increaseCap(StackItem *items)
 {
     capacity_ *= 2;
@@ -87,7 +88,7 @@ void DynamicStack::increaseCap(StackItem *items)
     delete[] items_;
     items_ = newItems;
 }
-
+// halves the capacity of the stack when the size is 1/4 the capacity
 void DynamicStack::decreaseCap (StackItem *items)
 {
     capacity_ /= 2;
